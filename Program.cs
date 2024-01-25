4e7621c0b9b197473b408ea098ea226694a5b3f3
Bogdan Strinic <Bogdan.Strinic@edu.sait.ca>
2024-01-25 14:11:09 -0700

    Created methods for get age in 10 years and a string method to display all of the persons information.

--- Program.cs
+++ Program.cs
@@ -19,9 +19,20 @@ public void DisplayPersonalInfo()
     Console.WriteLine($"Name= {firstName} {lastName}\npersonId: {personID} Name's favorite colour is {favoriteColour}");
 }
 
-//Changing the favorite colour to white
+//Changing the favorite colour to white.
 public void ChangeFavoriteColour()
 {
     ChangeFavoriteColour = "White";
 }
 
+//Get the age after 10 years.
+public int GetAgeInTenYears()
+{
+    return age + 10;
+}
+
+//Overiding the ToString method to display the details of the information.
+public override string ToString()
+{
+    return $"Person Id: {personId}\nFirst Name: {firstName}\nLast Name: {lastName}\nFavorite Colour: {favoriteColour}\nAge: {age}\nIs Working: {IsWorking}";
+}
