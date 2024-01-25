c3d57f25d1599c1655c1a76ceba31f86ba9d1cd7
Bogdan Strinic <Bogdan.Strinic@edu.sait.ca>
2024-01-25 13:56:01 -0700

    Defined the Person class and also defined the attributes associated with it.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+//Defining the Person class. 
+class Person
+{
+    //Defining the attributes.
+    public int personID;
+    public string firstName;
+    public string lastName;
+    public string favoriteColour;
+    public int age;
+    public bool isWorking;
+}
